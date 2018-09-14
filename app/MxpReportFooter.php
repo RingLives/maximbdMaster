02fc122d3cb0c50ed9144e74bcd9e3ca65ec62bc
kamruzzaman <kamruzzaman@bookhint.com>
2018-09-14 17:26:49 +0600

    change text case

--- app/MxpReportFooter.php
+++ app/MxpReportFooter.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MxpReportFooter extends Model
 {
-    protected $table = 'mxp_reportFooter';
+    protected $table = 'mxp_reportfooter';
 	protected $primaryKey = 're_footer_id';
 
 	protected $fillable = ['user_id','reportName','description_1','description_2','description_3','description_4','description_5','siginingPerson_1','siginingPerson_2','siginingPersonSeal_1','siginingSignature_1','siginingSignature_2','siginingPersonSeal_2','status','action'];
